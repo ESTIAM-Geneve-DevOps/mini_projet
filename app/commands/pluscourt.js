223d2526d76607e4b522a897f0f4d340ef73d237
Nicolas <nicolas.lavergne@estiam.com>
2023-01-24 11:24:54 +0100

    Ajout des commandes dans le script principal

--- app/commands/pluscourt.js
+++ app/commands/pluscourt.js
@@ -1,11 +1,10 @@
 function pluscourt(chaine)
 {
     var array = chaine.split(' ');
-    var smaller = array.reduce(function(a, b) {
+    return array.reduce(function(a, b) {
         return a.length <= b.length ? a : b;
     })
 
-    return chaine.replace(smaller, '');
 }
 
 module.exports = pluscourt;
--- app/script/terminal_input.js
+++ app/script/terminal_input.js
@@ -1,16 +1,38 @@
 function terminal_input(input) {
-    if (input == null) {
-      var input = document.getElementById("inputCommand").value;
-    }
-    var command = input.split(" ")[0];
-    var argument = input.split(" ")[1];
-    switch (command) {
-      case "premier":
-        alert("Commande premier");
-        premier(argument)
-        break;
-      default:
-        alert("Commande non reconnue");
-    }
-    return false;
+  if (input == null) {
+    var input = document.getElementById("inputCommand").value;
+  }
+  var command = input.split(" ")[0];
+  var argument = input.split(" ")[1];
+  switch (command) {
+    case "premier":
+      var result = premier(argument);
+      alert(result);
+      break;
+    case "airecercle":
+      var result = airecercle(argument);
+      alert(result);
+      break;
+    case "estpair":
+      var result = estpair(argument);
+      alert(result);
+      break;
+    case "pluscourt":
+      
+      var result = pluscourt(argument);
+      alert(result);
+      break;
+    case "sansvoyelle":
+      var result = sansvoyelle(argument);
+      alert(result);
+      break;
+    case "somme":
+      var result = somme(argument);
+      alert(result);
+      break;
+
+    default:
+      alert("Commande non reconnue");
+  }
+  return false;
 }
